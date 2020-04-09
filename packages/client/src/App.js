ebe78039612612c94c1f381237dad48e94b180a4
Stefan Nieuwenhuis <snieuwenhuis@bol.com>
2020-04-09 22:09:53 +0200

    Initial Commit

---
+++ packages/client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <my-component first='Stefan'></my-component>
+    </div>
+  );
+}
+
+export default App;
